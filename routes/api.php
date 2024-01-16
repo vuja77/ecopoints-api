e14d634818331781717de511e9d1f1336ad062cb
Vuja <djordjijevujovic210306@gmail.com>
2024-01-16 01:39:37 +0100

    api routes

--- routes/api.php
+++ routes/api.php
@@ -16,11 +16,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [Controllers\UserController::class, 'login']);
 Route::post('register', [Controllers\UserController::class, 'register']);
+Route::resource('categories', Controllers\CategoryController::class);
+Route::resource('products', Controllers\ProductController::class);
+Route::resource('sponsor', Controllers\SponsorController::class);
+Route::resource('qrcodes', Controllers\ScanQrcodeController::class);
 Route::get('a', function () {
     return "not authenticated";
 })->name("a");
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('details', [Controllers\UserController::class, 'details'])->name("details");
-
+    Route::post('buy', [Controllers\ProductController::class, 'buy']);
+    Route::get('PurchasedProduct', [Controllers\ProductController::class, 'buyedProduct']);
+    Route::post('points', [Controllers\PointsController::class, "addPoints"]);
+    Route::resource('favorite', Controllers\FavoriteController::class);
 });
